f896ca74a01668b93130114f8d06632779208654
GiftyL <giftylelissa@gmail.com>
2022-03-10 12:23:09 -0800

    commit tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-/*
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
-      */
+
+/**
+ *main - Entry point
+ *description: entry point
+ * return: Always 0 (Success)
+ */
+
 int main(void)
+
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-                return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+
 }
